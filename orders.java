891b36eed44ea5c332504a0a2836e372387b78af
saireddyj <saiprasadj70@gmail.com>
2026-01-22 15:41:19 +0530

    Update orders.java

--- orders.java
+++ orders.java
@@ -2,6 +2,7 @@ public class login{
   private string username;
   private string password;
   public string login (string username , string password)
+  // bug1 fixed
   {
     return "suc"
       }
